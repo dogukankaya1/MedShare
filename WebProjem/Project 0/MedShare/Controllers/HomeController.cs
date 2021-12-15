0cfc81957f879db6d5d389d0d9021b97dcd2ad84
dogukankaya1 <82560818+dogukankaya1@users.noreply.github.com>
2021-12-16 00:18:31 +0300

    Add files via upload
    Very first day of the project.

---
+++ WebProjem/Project 0/MedShare/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using MedShare.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+/* Idea: İnsanların ellerinde fazla kalan ilaçları paylaşabilmeleri için,
+ * ilaç eklenen ve bireylerin ellerinde kaç adet o ilaçtan oldugunu ne kadar 
+ * gönderebileceklerini yorumlayıp database e ekleyebildikleri bir sistem. 
+ * 3 kutu simflatım var fazla diyip ekliyorum, elde 3 kutu var gözüküyor,
+ * başka biri de buradan cekiyor mesela gibi. 
+ * Her biri 3 kutu ekledikce database değişiyor. 
+ */
+
+namespace MedShare.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebProjem/Project 0/MedShare/Models/Med.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedShare.Models
+{
+    public class Med
+    {
+        public int BarkodNo;
+        public string DrugName;
+        public string Producer;
+    }
+}
